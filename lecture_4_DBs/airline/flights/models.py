f712c606612316a0702764084044b30f13db24b0
Andre Nel <acnel@s-plane.com>
2024-02-17 19:56:34 +0200

    Add Airport Model

--- lecture_4_DBs/airline/flights/models.py
+++ lecture_4_DBs/airline/flights/models.py
@@ -2,11 +2,22 @@ from django.db import models
 
 
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"{self.city} to {self.code}"
+
+
 # python manage.py makemigrations
 # python manage.py migrate
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    # related_name when you want to access the flight information from the Airport class
+    origin = models.ForeignKey(to=Airport, on_delete=models.CASCADE,
+                               related_name="departures")
+    destination = models.ForeignKey(to=Airport, on_delete=models.CASCADE,
+                                    related_name="arrivals")
     duration = models.IntegerField()
 
     # Returns a string representation of the object
